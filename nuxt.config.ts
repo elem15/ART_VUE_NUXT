cba6f9ccbbea33fdff01b1190e4d2964fb4b7438
Elem <elem15@yandex.ru>
2023-08-07 23:12:50 +0300

    feat: add yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,16 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/devtools',
     'nuxt-swiper',
-    '@nuxtjs/supabase'
+    '@nuxtjs/supabase',
+    'yandex-metrika-module-nuxt3'
   ],
+  yandexMetrika: {
+    id: '61602646',
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+  },
   image: {
     inject: true,
     quality: 80,
