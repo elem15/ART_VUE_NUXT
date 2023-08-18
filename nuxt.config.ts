41d2175eb4c8bb650be42dc2f4094627e05bdb72
Elem <elem15@yandex.ru>
2023-08-18 20:05:20 +0300

    feat: disable dark-reader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [{ name: 'darkreader-lock' }]
+    }
+  },
   devtools: { enabled: true, vscode: {} },
   modules: [
     '@nuxtjs/eslint-module',
