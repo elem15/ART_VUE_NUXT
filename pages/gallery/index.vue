b3038a2b26d5d22a1b35a75b7cc759ae829bb24f
Elem <elem15@yandex.ru>
2023-09-06 17:43:49 +0300

    feat: move footer below

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -3,46 +3,48 @@
     <div v-if="loading" class="fog">
       <SpinnerView />
     </div>
-    <div v-if="galleries.length" class="gallery-wrapper-styles" :class="loading && 'hidden'">
-      <ul class="box-container three-cols gallery gallery-styles">
-        <li v-for="(gallery, idx) in galleries" :key="gallery.id" class="box">
-          <div class="inner">
-            <NuxtLink :to="`/gallery/${gallery.title}`" class="glightbox">
-              <nuxt-img
-                :src="gallery.src"
-                :alt="gallery.alt"
-                width="250"
-                height="auto"
-                loading="lazy"
-                preload
-                class="art-styles"
-                @load="() => { if(idx === galleries.length -1) onLoadEvent() }"
-              />
-              <h4 class="picture-styles">
-                {{ gallery.alt }}
-              </h4>
+    <div class="wrapper">
+      <div v-if="galleries.length" class="gallery-wrapper-styles" :class="loading && 'hidden'">
+        <ul class="box-container three-cols gallery gallery-styles">
+          <li v-for="(gallery, idx) in galleries" :key="gallery.id" class="box">
+            <div class="inner">
+              <NuxtLink :to="`/gallery/${gallery.title}`" class="glightbox">
+                <nuxt-img
+                  :src="gallery.src"
+                  :alt="gallery.alt"
+                  width="250"
+                  height="auto"
+                  loading="lazy"
+                  preload
+                  class="art-styles"
+                  @load="() => { if(idx === galleries.length -1) onLoadEvent() }"
+                />
+                <h4 class="picture-styles">
+                  {{ gallery.alt }}
+                </h4>
+              </NuxtLink>
+            </div>
+          </li>
+        </ul>
+        <hr>
+        <div class="picture-right">
+          <div>
+            <NuxtLink to="/about">
+              <nuxt-img src="https://umlxyrmekufynqaatflf.supabase.co/storage/v1/object/public/artist/vadiy.jpg" alt="Vadiy" />
             </NuxtLink>
           </div>
-        </li>
-      </ul>
-      <hr>
-      <div class="picture-right">
-        <div>
-          <NuxtLink to="/about">
-            <nuxt-img src="https://umlxyrmekufynqaatflf.supabase.co/storage/v1/object/public/artist/vadiy.jpg" alt="Vadiy" />
-          </NuxtLink>
-        </div>
-        <div class="picture-description">
-          <h3 class="picture-description-header">
-            <a href="about-artist.html">о художнике</a>
-          </h3>
-          <p class="picture-description-article">
-            Описание работ
-          </p>
+          <div class="picture-description">
+            <h3 class="picture-description-header">
+              <a href="about-artist.html">о художнике</a>
+            </h3>
+            <p class="picture-description-article">
+              Описание работ
+            </p>
+          </div>
         </div>
       </div>
+      <FooterView :loading="loading" />
     </div>
-    <FooterView :loading="loading" />
   </main>
 </template>
 
@@ -89,6 +91,12 @@ if (data?.length) {
 .gallery img {
   box-shadow: -10px 10px 20px -5px rgb(93, 65, 65);
   border-radius: 5px;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 83vh;
 }
 
 </style>
